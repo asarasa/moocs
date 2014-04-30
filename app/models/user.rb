e093c33709707d493d25106912833dc152d27b1a
Félix Martín <nitram6x@gmail.com>
2014-04-30 14:20:06 +0200

    Try to fix the bug with the default url files

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User
   has_many :resources
   has_many :tests
   
-  has_mongoid_attached_file :photo, :default_url => ActionController::Base.helpers.asset_path("buddyicon.jpg", :digest => false);
+  has_mongoid_attached_file :photo, :default_url => ActionController::Base.helpers.asset_path("buddyicon.jpg");
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   validates_presence_of :name, :lastname, :email, :password_digest
